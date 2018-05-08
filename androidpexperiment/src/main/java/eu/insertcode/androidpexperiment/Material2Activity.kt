e0e75d40737473b3c589d651cf9dff3f90552493
MrTheGood <maarten@degoedeweb.nl>
2018-05-08 19:00:11 +0200

    Add BottomAppBar

--- androidpexperiment/src/main/java/eu/insertcode/androidpexperiment/Material2Activity.kt
+++ androidpexperiment/src/main/java/eu/insertcode/androidpexperiment/Material2Activity.kt
@@ -2,11 +2,14 @@ package eu.insertcode.androidpexperiment
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_material_2.*
 
 class Material2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_material_2)
+
+        appbar.replaceMenu(R.menu.menu)
     }
 }
